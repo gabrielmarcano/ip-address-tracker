b62cb344095052059de11ca0cf030d433b00b3a2
Gabriel Marcano <gabrielmarcano120@gmail.com>
2025-08-16 14:22:51 -0400

    Packages for the map

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,8 +1,29 @@
+import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet'
+
 function Map() {
+  const position: [number, number] = [51.505, -0.09] // Default position
+
   return (
     <div className="@map h-80 w-full bg-black">
       <h1>Map Component</h1>
       {/* Map rendering logic will go here */}
+    </div>
+  )
+
+  return (
+    <div className="@map h-10 w-10">
+      <MapContainer center={position} zoom={13} scrollWheelZoom={false}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+        <Marker position={position}>
+          <Popup>
+            A pretty CSS3 popup. <br /> Easily customizable.
+          </Popup>
+        </Marker>
+      </MapContainer>
+      ,
     </div>
   )
 }
